ce6029c2809faaa4e03c162e7b059d326226875b
Rahul Umesh Mhatre <rahul.mhatre@phenompeople.com>
2023-05-14 22:49:21 +0530

    Q1 echo solved

---
+++ HackerRank/Linux-Shell/01_Echo.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Write a bash script that prints the string "HELLO".
+# https://www.hackerrank.com/challenges/bash-tutorials-lets-echo/problem?isFullScreen=true
+
+echo "Sloution:"
+
+echo "HELLO"
